62a69ecd1a40ef434f0ab495b8dd6cacad65b89f
Halfwhit <Halfwhit@gmail.com>
2022-05-16 16:01:30 +0100

    Started genetic algorithm

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,5 +1,23 @@
+pub struct GeneticAlgorithm;
+
+impl GeneticAlgorithm {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn evolve<I>(&self, population &[I]) -> Vec<I> {
+        assert!(!population.is_empty());
+
+        (0..population.len())
+            .map(|_| {
+                todo!()
+            })
+            .collect()
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod genetic_algorithm {
     #[test]
     fn it_works() {
         let result = 2 + 2;
