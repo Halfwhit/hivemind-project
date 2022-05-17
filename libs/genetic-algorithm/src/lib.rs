e583fb8d0dcfe3d1f7a59d225a44abc253f8a361
Halfwhit <Halfwhit@gmail.com>
2022-05-17 12:52:54 +0100

    Added selection criteria

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,15 +1,69 @@
-pub struct GeneticAlgorithm;
+use rand::prelude::*;
 
-impl GeneticAlgorithm {
+pub struct GeneticAlgorithm<S> {
+    selection_method: S,
+}
+
+pub trait Individual {
+    fn fitness(&self) -> f32;    
+}
+
+pub trait SelectionMethod {
+    fn select<'a, I>(
+        &self,
+        rng: &mut dyn RngCore,
+        population: &'a [I],
+    ) -> &'a I
+    where
+        I: Individual;
+}
+
+pub struct RouletteWheelSelection;
+
+impl  RouletteWheelSelection {
     pub fn new() -> Self {
         Self
+    }    
+}
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(
+        &self, 
+        rng: &mut dyn RngCore, 
+        population: &'a [I]
+    ) -> &'a I
+    where
+        I: Individual 
+    {
+        population.choose_weighted(rng, |individual| individual.fitness())
+            .expect("Empty population")
+    }
+}
+
+impl<S> GeneticAlgorithm<S> 
+where
+    S: SelectionMethod,
+{
+    pub fn new(selection_method: S) -> Self {
+        Self { selection_method }
     }
 
-    pub fn evolve<I>(&self, population &[I]) -> Vec<I> {
+    pub fn evolve<I>(
+        &self, 
+        rng: &mut dyn RngCore,
+        population: &[I]
+    ) -> Vec<I> 
+    where
+        I: Individual,
+    {
         assert!(!population.is_empty());
 
         (0..population.len())
             .map(|_| {
+                let parent_a = self.selection_method.select(rng, population);
+                let parent_b = self.selection_method.select(rng, population);
+                // Crossover
+                // Mutation
                 todo!()
             })
             .collect()
@@ -17,10 +71,60 @@ impl GeneticAlgorithm {
 }
 
 #[cfg(test)]
+#[derive(Clone, Debug)]
+pub struct TestIndividual {
+    fitness: f32,
+}
+
+#[cfg(test)]
+impl TestIndividual {
+    pub fn new(fitness: f32) -> Self {
+        Self { fitness }
+    }
+}
+
+#[cfg(test)]
+impl Individual for TestIndividual {
+    fn fitness(&self) -> f32 {
+        self.fitness
+    }
+}
+
+#[cfg(test)]
 mod genetic_algorithm {
+    use std::collections::BTreeMap;
+
+    use super::*;
+    use rand_chacha::ChaCha8Rng;
+    
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn roulette_selection() {
+        let method = RouletteWheelSelection::new();
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+
+        let actual_histogram: BTreeMap<i32, _> = (0..1000)
+            .map(|_| method.select(&mut rng, &population))
+            .fold(Default::default(), |mut histogram, individual| {
+                *histogram.entry(individual.fitness() as _)
+                    .or_default() += 1;
+                
+                histogram
+            });
+
+        let expected_histogram = maplit::btreemap! {
+            1 => 98,
+            2 => 202,
+            3 => 278,
+            4 => 422,
+        };
+
+        assert_eq!(actual_histogram, expected_histogram);
     }
 }
