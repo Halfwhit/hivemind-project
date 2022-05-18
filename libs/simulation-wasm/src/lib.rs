da5decbd7c9b760f8f1d4d306754526ea81330f0
Halfwhit <Halfwhit@gmail.com>
2022-05-18 15:57:00 +0100

    wasm-bindgen

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,8 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn whos_that_dog() -> String {
+    "Mister Peanutbutter".into()
 }
