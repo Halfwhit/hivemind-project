6494c323a5d54227b89169553713bcdaed6480ab
Halfwhit <Halfwhit@gmail.com>
2022-05-19 14:21:26 +0100

    www

---
+++ www/index.js
@@ -0,0 +1,55 @@
+import * as sim from "lib-simulation-wasm";
+
+const simulation = new sim.Simulation();
+const world = simulation.world();
+
+const viewport = document.getElementById('viewport');
+
+const viewportWidth = viewport.width;
+const viewportHeight = viewport.height;
+
+const viewportScale = window.devicePixelRatio || 1;
+
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+
+viewport.style.width = viewportWidth + 'px';
+viewport.style.height = viewportHeight + 'px';
+
+const ctxt = viewport.getContext('2d');
+
+ctxt.scale(viewportScale, viewportScale);
+
+ctxt.fillStyle = 'rgb(0, 0, 0)';
+
+CanvasRenderingContext2D.prototype.drawTriangle = function drawTriangle(x, y, size, rotation) {
+    ctxt.beginPath();
+    this.moveTo(
+        x + Math.cos(rotation) * size * 1.5,
+        y + Math.sin(rotation) * size * 1.5,
+    );
+    this.lineTo(
+        x + Math.cos(rotation + 2.0 / 3.0 * Math.PI) * size,
+        y + Math.sin(rotation + 2.0 / 3.0 * Math.PI) * size,
+    );
+    this.lineTo(
+        x + Math.cos(rotation + 4.0 / 3.0 * Math.PI) * size,
+        y + Math.sin(rotation + 4.0 / 3.0 * Math.PI) * size,
+    );
+    this.lineTo(
+        x + Math.cos(rotation) * size * 1.5,
+        y + Math.sin(rotation) * size * 1.5,
+    );
+
+    ctxt.stroke();
+};
+
+
+for (const animal of simulation.world().animals) {
+    ctxt.drawTriangle(
+        animal.x * viewportWidth,
+        animal.y * viewportHeight,
+        0.01 * viewportWidth,
+        animal.rotation
+    );
+};
