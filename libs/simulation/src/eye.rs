349b39c0ae16cd5258569d3f63db81a094620008
Halfwhit <Halfwhit@gmail.com>
2022-05-20 14:53:04 +0100

    eyes

--- libs/simulation/src/eye.rs
+++ libs/simulation/src/eye.rs
@@ -75,15 +75,178 @@ impl Default for Eye {
 mod tests {
     use super::*;
 
+    struct TestCase {
+        foods: Vec<Food>,
+        fov_range: f32,
+        fov_angle: f32,
+        x: f32,
+        y: f32,
+        rot: f32,
+        expected_vision: &'static str
+    }
+
+    const TEST_EYE_CELLS: usize = 13;
+
+    impl TestCase {
+        fn run(self) {
+            let eye = Eye::new(
+                self.fov_range,
+                self.fov_angle,
+                TEST_EYE_CELLS
+            );
+
+            let actual_vision = eye.process_vision(
+                na::Point2::new(self.x, self.y),
+                na::Rotation2::new(self.rot),
+                &self.foods
+            );
+
+        let actual_vision: Vec<_> = actual_vision
+            .into_iter()
+            .map(|cell| {
+                if cell >= 0.7 {"#"}
+                else if cell >= 0.3 {"+"}
+                else if cell > 0.0 {"."}
+                else {" "}
+            })
+            .collect();
+        
+        let actual_vision = actual_vision.join("");
+
+        assert_eq!(actual_vision, self.expected_vision);
+        }
+    }
+
+    fn food(x: f32, y: f32) -> Food {
+        Food { position: na::Point2::new(x, y) }
+    }
+
     mod different_fov_ranges {
         use super::*;
         use test_case::test_case;
 
-        #[test_case(1.0)]
-        #[test_case(0.5)]
-        #[test_case(0.1)]
-        fn test(fov_range: f32) {
-            todo!()
+        #[test_case(1.0, "      +      ")]
+        #[test_case(0.9, "      +      ")]
+        #[test_case(0.8, "      +      ")]
+        #[test_case(0.7, "      .      ")]
+        #[test_case(0.6, "      .      ")]
+        #[test_case(0.5, "             ")]
+        #[test_case(0.4, "             ")]
+        #[test_case(0.3, "             ")]
+        #[test_case(0.2, "             ")]
+        #[test_case(0.1, "             ")]
+        fn test(fov_range: f32, expected_vision: &'static str) {
+            TestCase {
+                foods: vec![food(1.0, 0.5)],
+                fov_angle: FRAC_PI_2,
+                x: 0.5,
+                y: 0.5,
+                rot: 0.0,
+                fov_range,
+                expected_vision
+            }.run()
+        }
+    }
+
+    mod different_rotations {
+        use super::*;
+        use test_case::test_case;
+
+        #[test_case(0.00 * PI, "         +   ")]
+        #[test_case(0.25 * PI, "        +    ")]
+        #[test_case(0.50 * PI, "      +      ")]
+        #[test_case(0.75 * PI, "    +        ")]
+        #[test_case(1.00 * PI, "   +         ")]
+        #[test_case(1.25 * PI, " +           ")]
+        #[test_case(1.50 * PI, "            +")]
+        #[test_case(1.75 * PI, "           + ")]
+        #[test_case(2.00 * PI, "         +   ")]
+        #[test_case(2.25 * PI, "        +    ")]
+        #[test_case(2.50 * PI, "      +      ")]
+        fn test(rot: f32, expected_vision: &'static str) {
+            TestCase {
+                foods: vec![food(0.5, 1.0)],
+                fov_range: 1.0,
+                fov_angle: 2.0 * PI,
+                x: 0.5,
+                y: 0.5,
+                rot,
+                expected_vision,
+            }.run()
+        }
+    }
+
+    mod different_positions {
+        use super::*;
+        use test_case::test_case;
+
+        // Checking the X axis:
+        #[test_case(0.9, 0.5, "#           #")]
+        #[test_case(0.8, 0.5, "  #       #  ")]
+        #[test_case(0.7, 0.5, "   +     +   ")]
+        #[test_case(0.6, 0.5, "    +   +    ")]
+        #[test_case(0.5, 0.5, "    +   +    ")]
+        #[test_case(0.4, 0.5, "     + +     ")]
+        #[test_case(0.3, 0.5, "     . .     ")]
+        #[test_case(0.2, 0.5, "     . .     ")]
+        #[test_case(0.1, 0.5, "     . .     ")]
+        #[test_case(0.0, 0.5, "             ")]
+
+        // Checking the Y axis:
+        #[test_case(0.5, 0.0, "            +")]
+        #[test_case(0.5, 0.1, "          + .")]
+        #[test_case(0.5, 0.2, "         +  +")]
+        #[test_case(0.5, 0.3, "        + +  ")]
+        #[test_case(0.5, 0.4, "      +  +   ")]
+        #[test_case(0.5, 0.6, "   +  +      ")]
+        #[test_case(0.5, 0.7, "  + +        ")]
+        #[test_case(0.5, 0.8, "+  +         ")]
+        #[test_case(0.5, 0.9, ". +          ")]
+        #[test_case(0.5, 1.0, "+            ")]
+        fn test(x: f32, y: f32, expected_vision: &'static str) {
+            TestCase {
+                foods: vec![food(1.0, 0.4), food(1.0, 0.6)],
+                fov_range: 1.0,
+                fov_angle: FRAC_PI_2,
+                rot: 0.0,
+                x,
+                y,
+                expected_vision,
+            }.run()
+        }
+    }
+
+    mod different_fov_angles {
+        use super::*;
+        use test_case::test_case;
+
+        #[test_case(0.25 * PI, " +         + ")] // FOV is narrow = 2 foods
+        #[test_case(0.50 * PI, ".  +     +  .")]
+        #[test_case(0.75 * PI, "  . +   + .  ")] // FOV gets progressively
+        #[test_case(1.00 * PI, "   . + + .   ")] // wider and wider...
+        #[test_case(1.25 * PI, "   . + + .   ")]
+        #[test_case(1.50 * PI, ".   .+ +.   .")]
+        #[test_case(1.75 * PI, ".   .+ +.   .")]
+        #[test_case(2.00 * PI, "+.  .+ +.  .+")] // FOV is wide = 8 foods
+        fn test(fov_angle: f32, expected_vision: &'static str) {
+            TestCase {
+                foods: vec![
+                    food(0.0, 0.0),
+                    food(0.0, 0.33),
+                    food(0.0, 0.66),
+                    food(0.0, 1.0),
+                    food(1.0, 0.0),
+                    food(1.0, 0.33),
+                    food(1.0, 0.66),
+                    food(1.0, 1.0),
+                ],
+                fov_range: 1.0,
+                x: 0.5,
+                y: 0.5,
+                rot: 0.0,
+                fov_angle,
+                expected_vision,
+            }.run()
         }
     }
 }
